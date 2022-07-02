12eed15e3b69f7590a0ef43a3289db0e47342f66
Naomi Carey <naomicarey00@gmail.com>
2022-07-02 10:34:27 -0600

    added global state

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,49 +1,55 @@
-import {useQuery} from 'urql';
-import {GET_PRODUCT_QUERY} from '../../lib/query';
-import { useRouter } from 'next/router';
-import {DetailsStyle, ProductInfo, Quantity, Buy} from '../../styles/ProductDetails';
-import {AiFillPlusCircle, AiFillMinusCircle} from 'react-icons/ai';
-
+import { useQuery } from "urql";
+import { GET_PRODUCT_QUERY } from "../../lib/query";
+import { useRouter } from "next/router";
+import {
+  DetailsStyle,
+  ProductInfo,
+  Quantity,
+  Buy,
+} from "../../styles/ProductDetails";
+import { AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+import { useStateContext } from "../../lib/context";
 
 export default function ProductDetails() {
-//fetch slug
-const {query} = useRouter();
+  //Use state
+  const { qty, increaseQty, decreaseQty } = useStateContext();
 
-    //fetch graphql data
-    const [results] = useQuery({
+  //fetch slug
+  const { query } = useRouter();
+
+  //fetch graphql data
+  const [results] = useQuery({
     query: GET_PRODUCT_QUERY,
-    variables: {slug: query.slug}
-});
+    variables: { slug: query.slug },
+  });
+  const { data, fetching, error } = results;
 
-const {data, fetching, error} = results;
-//Check for the data coming in
-if(fetching) return <p>Loading....</p>
- if(error) return <p>Oh no....{error.messsage}</p>
-//extract our data
+  //Check for the data coming in
+  if (fetching) return <p>Loading....</p>;
+  if (error) return <p>Oh no....{error.messsage}</p>;
 
-const {title, description, image} = data.products.data[0].attributes;
+  //extract our data
+  const { title, description, image } = data.products.data[0].attributes;
 
   return (
-  
-  <DetailsStyle>
-    <img src={image.data.attributes.formats.medium.url} alt={title}/>
-    <ProductInfo>
+    <DetailsStyle>
+      <img src={image.data.attributes.formats.medium.url} alt={title} />
+      <ProductInfo>
         <h3>{title}</h3>
         <p>{description}</p>
 
-    <Quantity>
-        <span>Quantity</span>
-        <button><AiFillMinusCircle/></button>
-        <p>0</p>
-        <button><AiFillPlusCircle/></button>
-    </Quantity>
-    <Buy>Add to cart</Buy>
-    
-    </ProductInfo>
-    
-    
-  
-  </DetailsStyle>
-  
+        <Quantity>
+          <span>Quantity</span>
+          <button>
+            <AiFillMinusCircle onClick={decreaseQty} />
+          </button>
+          <p>{qty}</p>
+          <button>
+            <AiFillPlusCircle onClick={increaseQty} />
+          </button>
+        </Quantity>
+        <Buy>Add to cart</Buy>
+      </ProductInfo>
+    </DetailsStyle>
   );
 }
